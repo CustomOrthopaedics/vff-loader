3b94b6119cdbd1388c91bbb679ebdc43f8541ff0
Dan Kaplun <dkaplun@customorthopaedics.com>
2018-04-27 12:31:20 -0400

    Test geometry parsing

--- test/VFFLoader.test.js
+++ test/VFFLoader.test.js
@@ -1,8 +1,27 @@
+import fs from 'fs';
+import path from 'path';
+import { promisify } from 'util';
+
+import * as THREE from 'three';
+
 import VFFLoader from '..';
+
+const readFileAsync = promisify(fs.readFile);
+async function fixture(name, ...args) {
+  return readFileAsync(path.join(__dirname, 'fixtures', name), ...args);
+}
 
 describe('VFFLoader', () => {
   it('should be a constructor', () => {
     const loader = new VFFLoader();
     expect(loader).toBeInstanceOf(VFFLoader);
+  });
+
+  it('should parse a geometry from a .vff', async () => {
+    const loader = new VFFLoader();
+
+    const geom = loader.parse(await fixture('airway.vff'));
+    expect(geom).toBeInstanceOf(THREE.Geometry);
+    expect(geom.vertices).toHaveLength(36785);
   });
 });
