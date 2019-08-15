5fd124fca686a15a3645b038884a38cdf9ef6845
Brian Beckrest <beckrestb@gmail.com>
2019-08-15 15:02:06 -0400

    Upgrade to babel v7

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: '8.9.4',
+          browsers: 'last 2 versions',
+        },
+        useBuiltIns: 'usage',
+        corejs: 3,
+      },
+    ],
+  ],
+  plugins: [],
+};
